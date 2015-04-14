a2dae56e0823218a93946a39e04361f15fecb422
Byron Peña <byronpenna@gmail.com>
2015-04-14 11:09:18 -0600

    Diseño de pagina traduccion :relaxed:

--- IUS/IUSBack/Scripts/Views/GestionIdiomaWebsite/script.js
+++ IUS/IUSBack/Scripts/Views/GestionIdiomaWebsite/script.js
@@ -1,3 +1,12 @@
 ﻿$(document).ready(function () {
-    alert("holaa");
+    // plugins
+        // chosen
+            $(".cbLlave").chosen({ no_results_text: "Esa llave no existe", width: '100%' });
+            $(".cbPagina").chosen({ no_results_text: "La pagina no existe", width: '100%' });
+            $(".cbIdioma").chosen({ no_results_text: "El idioma no existe", width: '100%' });
+    // eventos  
+        // click
+            
+        // change
+
 });
