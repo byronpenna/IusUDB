b8dc521d85eb529f0db17abb16a0b1728d68e7b2
Byron Peña <byronpenna@gmail.com>
2015-04-21 17:15:43 -0600

    Finalizacion de jornada

--- IUS/IUSBack/Scripts/Views/Administracion/Eventos/script.js
+++ IUS/IUSBack/Scripts/Views/Administracion/Eventos/script.js
@@ -1,14 +1,14 @@
 ﻿$(document).ready(function () {
     // plugins 
         // full calendar
-    $("#calendar").fullCalendar({
-        editable: true,
-    });
-    /*eventData = {
-        title: 'eventoPrueba',
-        start: '2015-04-01',
-        end: '2015-04-05'
-    };
-    $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true*/
+            $("#calendar").fullCalendar({
+                editable: true,
+            });
+        // tabs
+            $('#horizontalTab').responsiveTabs();
+    // funciones iniciales 
+        eventosIniciales();
+    
+    
 
 });
