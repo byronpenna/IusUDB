2f61b9f300a7da7d834c85773d43eee4cdf094d1
Byron Peña <byronpenna@gmail.com>
2015-04-21 15:54:18 -0600

    Instalacion de fullcalendar

--- IUS/IUSBack/Scripts/Views/Administracion/Eventos/script.js
+++ IUS/IUSBack/Scripts/Views/Administracion/Eventos/script.js
@@ -1,3 +1,14 @@
 ﻿$(document).ready(function () {
+    // plugins 
+        // full calendar
+    $("#calendar").fullCalendar({
+        editable: true,
+    });
+    /*eventData = {
+        title: 'eventoPrueba',
+        start: '2015-04-01',
+        end: '2015-04-05'
+    };
+    $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true*/
 
 });
