afe350849f23941f2cdb4ca23ff122553eecfbec
Byron Peña <byronpenna@gmail.com>
2015-05-05 18:14:27 -0600

    finalizacion de jornada

---
+++ IUS/IUSLibs/ADMINFE/Entidades/Valor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IUSLibs.ADMINFE.Entidades
+{
+    public class Valor
+    {
+        #region "propiedades"
+            public int              _idValor;
+            public string           _valor;
+            public Configuracion    _configuracion;
+        #endregion
+        #region "constructores"
+            public Valor(int idValor,string valor,int idConfiguracion)
+            {
+                this._idValor = idValor;
+                this._valor = valor;
+                Configuracion config = new Configuracion(idValor);
+                this._configuracion = config;
+            }
+        #endregion
+    }
+}
