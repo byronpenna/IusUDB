2b986610add40b54e0b73a19c80fe4090d299389
Byron Peña <byronpenna@gmail.com>
2015-05-08 07:08:21 -0600

    finalizacion de jornada

--- IUS/IUSBack/Content/third-party/datatable/js/jquery.dataTables.js
+++ IUS/IUSBack/Content/third-party/datatable/js/jquery.dataTables.js
@@ -14914,4 +14914,49 @@
 }));
 
 }(window, document));
+//----------------------Extra---------------------------
+jQuery.fn.dataTableExt.oApi.fnAddTr = function (oSettings, nTr, bRedraw) {
+    if (typeof bRedraw == 'undefined') {
+        bRedraw = true;
+    }
+
+    var nTds = nTr.getElementsByTagName('td');
+    if (nTds.length != oSettings.aoColumns.length) {
+        alert('Warning: not adding new TR - columns and TD elements must match');
+        return;
+    }
+
+    var aData = [];
+    var aInvisible = [];
+    var i;
+    // mete solo mi tr
+    console.log("a data es:", aData);
+    for (i = 0 ; i < nTds.length ; i++) {
+        console.log("data " + i + "es:", nTds[i].innerHTML);
+        aData.unshift(nTds[i].innerHTML);
+        if (!oSettings.aoColumns[i].bVisible) {
+            aInvisible.unshift(i);
+        }
+    }
+
+    /* Add the data and then replace DataTable's generated TR with ours */
+    var iIndex = this.oApi._fnAddData2(oSettings, aData);
+    nTr._DT_RowIndex = iIndex;
+    oSettings.aoData[iIndex].nTr = nTr;
+
+    oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
+
+    // Hidding invisible columns
+    for (i = (aInvisible.length - 1) ; i >= 0 ; i--) {
+        oSettings.aoData[iIndex]._anHidden[i] = nTds[aInvisible[i]];
+        nTr.removeChild(nTds[aInvisible[i]]);
+    }
+
+    // Redraw
+    if (bRedraw) {
+        this.oApi._fnReDraw(oSettings);
+    }
+};
+
+//--------------------------------------------------
 
