1e2d88130a4f0ee90e9dc927fc97e31f432f4b6e
Byron Peña <byronpenna@gmail.com>
2015-05-13 17:25:14 -0600

    finalizacion de jornada

--- IUS/IUSBack/Scripts/Generales/script.js
+++ IUS/IUSBack/Scripts/Generales/script.js
@@ -2,9 +2,10 @@
     $(document).on("keypress", ".soloLetras", function (e) {
         var str = String.fromCharCode(e.which);
         exp = soloLetras();
-        var x = test(exp,str);
+        var x = test(exp, str);
         if (!x) {
             e.preventDefault();
         }
-    })
+    });
+    // solo numeros
 });
--- IUS/IUSBack/Scripts/Views/Administracion/Noticias/script.js
+++ IUS/IUSBack/Scripts/Views/Administracion/Noticias/script.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(document).ready(function () {
+    
+    bkLib.onDomLoaded(function() {
+        txtAreaEditor = new nicEditor().panelInstance('editor');
+    })
+    //nicEditors.findEditor('editor').getContent();
+    $(document).on("keypress", ".txtTamañoImagen", function (e) {
+        console.log("string",String.fromCharCode(e.which));
+    })
+    $(document).on("click", ".nicEdit-main img", function () {
+        $(".activeRichImage").removeClass("activeRichImage");
+        $(this).addClass("activeRichImage");
+    })
+})
