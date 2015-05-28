6debdc052cfbbb47181e6341eda09285f0669936
Byron Peña <byronpenna@gmail.com>
2015-05-28 14:24:51 -0600

    slider

--- IUS/IUS/Scripts/views/Home/script.js
+++ IUS/IUS/Scripts/views/Home/script.js
@@ -2,6 +2,10 @@
     // iniciales
         setIdiomaPreferido();
     // eventos
+        // tap(slide)
+        $(document).on("swipe", ".imgSlider", function () {
+                console.log("Hizo tap");
+            })
         // click 
             $(document).on("click", ".navBtn", function () {
                 direccion = $(this).attr("direccion"); //0 izquierda 1 derecha
