ac2c214a1131c0b770792286d5cffcc3036c923f
Byron Peña <byronpenna@gmail.com>
2015-05-28 13:15:23 -0600

    funcionalidad hamburguerbutton

--- IUS/IUS/Scripts/Generales/script.js
+++ IUS/IUS/Scripts/Generales/script.js
@@ -1,4 +1,14 @@
 ﻿$(document).ready(function () {
     // Constantes
-        
+    $(document).on("click", ".desplegableMobile", function () {
+        ulMenu = $(this).parents(".mobileButton").parent().find(".ulMenu");
+
+        if (ulMenu.is(':visible')) {
+            console.log("visible")
+            ulMenu.hide();
+        } else {
+            console.log("no visible")
+            ulMenu.show();
+        }
+    });
 });
