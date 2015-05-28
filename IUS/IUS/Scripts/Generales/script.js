54b5a35df18fbc96a0230426527aed14c327a4ef
Byron Peña <byronpenna@gmail.com>
2015-05-28 13:51:48 -0600

    Final menu

--- IUS/IUS/Scripts/Generales/script.js
+++ IUS/IUS/Scripts/Generales/script.js
@@ -1,14 +1,15 @@
 ﻿$(document).ready(function () {
     // Constantes
-    $(document).on("click", ".desplegableMobile", function () {
-        ulMenu = $(this).parents(".mobileButton").parent().find(".ulMenu");
-
-        if (ulMenu.is(':visible')) {
-            console.log("visible")
-            ulMenu.hide();
-        } else {
-            console.log("no visible")
-            ulMenu.show();
-        }
-    });
+    // Eventos 
+        // click 
+            $(document).on("click", ".desplegableMobile", function () {
+                ulMenu = $(this).parents(".mobileButton").parent().find(".ulMenu");
+                if (ulMenu.is(':visible')) {
+                    console.log("visible")
+                    ulMenu.hide();
+                } else {
+                    console.log("no visible")
+                    ulMenu.show();
+                }
+            });
 });
