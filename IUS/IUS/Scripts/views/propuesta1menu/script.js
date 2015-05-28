657cec6f0a65cfa83029bc198d5af34af5cda86a
Byron Peña <byronpenna@gmail.com>
2015-05-28 14:39:16 -0600

    publicar

--- IUS/IUS/Scripts/views/propuesta1menu/script.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $(document).on("click", ".navBtn", function () {
-        var direccion = $(this).attr("direccion");
-        slide(direccion);
-    });
-})
--- IUS/IUS/Scripts/views/propuesta2/script.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    
-    $(document).on("click", ".nav", function () {
-        var direccion = $(this).attr("direccion");
-        slide(direccion);
-    });
-});
