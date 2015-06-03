27c9d09430f9f55e7b8804056fb0d6ccbdcd776a
Byron Peña <byronpenna@gmail.com>
2015-06-03 09:52:59 -0600

    Arreglos menores a noticias

---
+++ IUS/IUS/Scripts/views/Noticias/script.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    // eventos 
+        // click 
+            $(document).on("click", ".btnDesplegableAddComent", function () {
+                div = $(".agregarComentarioSection");
+                if (div.is(':visible')) {
+                    $(".spanIco i").removeClass("fa-angle-up");
+                    $(".spanIco i").addClass("fa-angle-down");
+                    div.hide("slow");
+                } else {
+                    $(".spanIco i").removeClass("fa-angle-down");
+                    $(".spanIco i").addClass("fa-angle-up");
+                    div.show("slow");
+                }
+            });
+});
