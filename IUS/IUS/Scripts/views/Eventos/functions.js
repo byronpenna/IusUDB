c10dc4e2eff84c5872575c1884383c5731da6bd5
Byron Peña <byronpenna@gmail.com>
2015-06-09 16:44:34 -0600

    antes de las busquedas de eventos

--- IUS/IUS/Scripts/views/Eventos/functions.js
+++ IUS/IUS/Scripts/views/Eventos/functions.js
@@ -1,4 +1,21 @@
 ﻿// acciones scripts
+    function ckAble(inputs, divPadre, ck) {
+        var able = false;
+        if (divPadre.parents(".rowControlesBusqueda").find("input:checked").length > 0) {
+            able = true;
+        }
+        if (ck.prop("checked")) {
+            inputs.prop("disabled", false);
+            able = true;
+        } else {
+            inputs.prop("disabled", true);
+        }
+        if (able) {
+            divPadre.parents(".seccionBusqueda").find(".botonBuscar").prop("disabled", false);
+        } else {
+            divPadre.parents(".seccionBusqueda").find(".botonBuscar").prop("disabled", true);
+        }
+    }
     function navEvento(actual, siguiente) {
         actual.fadeOut("slow", function () {
             siguiente.fadeIn("slow");
--- IUS/IUS/Scripts/views/Eventos/script.js
+++ IUS/IUS/Scripts/views/Eventos/script.js
@@ -1,30 +1,36 @@
 ﻿$(document).ready(function () {
     // eventos
         // click
-            $(document).on("click", ".btnDesplegarEventos", function () {
-                var divEventos = $(this).parents(".divSeparadorFecha").next();
-                console.log("entro");
-                btnDesplegarEventos(divEventos);
-            })
-            $(document).on("click", ".navEvento", function () {
-                pagina = $(this).parents(".divEventos").find(".pageEvent:visible");
-                var siguiente;
-                direccion = $(this).attr("direccion");
-                if (direccion == 0) {
-                    siguiente = pagina.prev(".pageEvent");
-                } else {
-                    siguiente = pagina.next(".pageEvent");
-                }
-                indefinida = !siguiente.hasClass("pageEvent");
-                if (indefinida && direccion == 0) {
-                    siguiente = pagina.parent().find(".pageEvent").last();
-                    console.log(siguiente.hasClass("pageEvent"));
-                    console.log("final");
-                } else if (indefinida && direccion == 1) {
-                    siguiente = pagina.parent().find(".pageEvent").first();
-                    console.log("Principio");
-                }
-                navEvento(pagina, siguiente);
-                
-            });
+            // busqueda
+                $(document).on("click", ".ckAble", function () {
+                    divPadre = $(this).parents(".divAtomBusqueda");
+                    inputs = divPadre.find(".inputsBusquedaSeccion input");
+                    ckAble(inputs, divPadre,$(this));
+                })
+            // eventos(tarjetitas)
+                $(document).on("click", ".btnDesplegarEventos", function () {
+                    var divEventos = $(this).parents(".divSeparadorFecha").next();
+                    console.log("entro");
+                    btnDesplegarEventos(divEventos);
+                })
+                $(document).on("click", ".navEvento", function () {
+                    pagina = $(this).parents(".divEventos").find(".pageEvent:visible");
+                    var siguiente;
+                    direccion = $(this).attr("direccion");
+                    if (direccion == 0) {
+                        siguiente = pagina.prev(".pageEvent");
+                    } else {
+                        siguiente = pagina.next(".pageEvent");
+                    }
+                    indefinida = !siguiente.hasClass("pageEvent");
+                    if (indefinida && direccion == 0) {
+                        siguiente = pagina.parent().find(".pageEvent").last();
+                        console.log(siguiente.hasClass("pageEvent"));
+                        console.log("final");
+                    } else if (indefinida && direccion == 1) {
+                        siguiente = pagina.parent().find(".pageEvent").first();
+                        console.log("Principio");
+                    }
+                    navEvento(pagina, siguiente);
+                });
 })
