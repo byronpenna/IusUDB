aa3cbb8966ec5f2e2f4153ed7cd67f07f9821169
Byron Peña <byronpenna@gmail.com>
2015-06-27 15:26:16 -0600

    mejora de funcionalidad insert

--- IUS/IUSBack/Scripts/Views/GestionInstituciones/functions.js
+++ IUS/IUSBack/Scripts/Views/GestionInstituciones/functions.js
@@ -1,4 +1,22 @@
-﻿// acciones script
+﻿// genericas 
+    function getTrInstituciones(institucion) {
+        tr = "\
+            <tr>\
+                <td>\
+                    <input class='txtHdIdInstitucion' name='txtHdIdInstitucion' value='"+institucion.idInstitucion+"'>\
+                </td>\
+                <td>"+institucion._nombre+"</td>\
+                <td>"+institucion._direccion+"</td>\
+                <td>"+institucion._pais._pais+"</td>\
+                <td>\
+                    <button class='btn'>Editar</button>\
+                    <button class='btn btnDeleteInstitucion'>Eliminar</button>\
+                </td>\
+            </tr>\
+        ";
+        return tr;
+    }
+// acciones script
     function btnDeleteInstitucion(frm,tr) {
         actualizarCatalogo(RAIZ + "/GestionInstituciones/sp_frontui_deleteInstitucion", frm, function (data) {
             console.log(data);
@@ -12,7 +30,8 @@
         actualizarCatalogo(RAIZ + "/GestionInstituciones/sp_frontui_insertInstitucion", frm, function (data) {
             console.log(data);
             if (data.estado) {
-
+                tr = getTrInstituciones(data.institucion);
+                $(".tbInstituciones").dataTable().fnAddTr($(tr)[0]);
             }
         })
     }
