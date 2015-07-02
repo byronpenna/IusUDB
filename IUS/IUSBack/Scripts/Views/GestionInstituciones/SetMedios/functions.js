878eb6fc4bafb1a4b1770c9ba6ec83a637be4ed3
Byron Peña <byronpenna@gmail.com>
2015-07-02 08:49:30 -0600

    cambio de tabla para edicion medios

--- IUS/IUSBack/Scripts/Views/GestionInstituciones/SetMedios/functions.js
+++ IUS/IUSBack/Scripts/Views/GestionInstituciones/SetMedios/functions.js
@@ -4,31 +4,39 @@
         trMedio.find(".txtTextoEnlaceEdit").val(medio.nombreEnlace);
         callback();
     }
-// acciones script
-    function btnActualizar(frm, trMedio) {
-        actualizarCatalogo(RAIZ + "/GestionMediosInstituciones/sp_frontui_editEnlaceInstitucion", frm, function (data) {
-            console.log(data);
-        });
+    function exitEditMode(trMedios,medio) {
+        trMedios.find(".tdEnlace").empty().append(medio._enlace);
+        trMedios.find(".tdNombreEnlace").empty().append(medio._nombreEnlace);
+        controlesEdit( false, trMedios);
     }
-    function btnEditar(trMedio) {
-        medio = {
-            enlace:         trMedio.find(".tdEnlace").text(),
-            nombreEnlace:   trMedio.find(".tdNombreEnlace").text(),
+    // acciones script
+        function btnActualizar(frm, trMedio) {
+            actualizarCatalogo(RAIZ + "/GestionMediosInstituciones/sp_frontui_editEnlaceInstitucion", frm, function (data) {
+                console.log(data);
+                if (data.estado) {
+                    exitEditMode(trMedio, data.enlace);
+                }
+            });
         }
-        fillInputsEdit(trMedio, medio, function () {
-            controlesEdit(true, trMedio);
-        });
-    }
-    function btnAgregar(frm) {
-        actualizarCatalogo(RAIZ + "/GestionMediosInstituciones/sp_frontui_insertEnlaceInstituciones", frm, function (data) {
-            console.log(data);
-        });
-    }
-    function btnEliminar(frm,tr) {
-        actualizarCatalogo(RAIZ + "/GestionMediosInstituciones/sp_frontui_deleteEnlaceInstitucion", frm, function (data) {
-            console.log(data);
-            if (data.estado) {
-                tr.remove();
+        function btnEditar(trMedio) {
+            medio = {
+                enlace:         trMedio.find(".tdEnlace").text(),
+                nombreEnlace:   trMedio.find(".tdNombreEnlace").text(),
             }
-        })
-    }
+            fillInputsEdit(trMedio, medio, function () {
+                controlesEdit(true, trMedio);
+            });
+        }
+        function btnAgregar(frm) {
+            actualizarCatalogo(RAIZ + "/GestionMediosInstituciones/sp_frontui_insertEnlaceInstituciones", frm, function (data) {
+                console.log(data);
+            });
+        }
+        function btnEliminar(frm,tr) {
+            actualizarCatalogo(RAIZ + "/GestionMediosInstituciones/sp_frontui_deleteEnlaceInstitucion", frm, function (data) {
+                console.log(data);
+                if (data.estado) {
+                    tr.remove();
+                }
+            })
+        }
