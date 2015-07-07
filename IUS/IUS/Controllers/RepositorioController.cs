dcc218dc2e4052a616416ea97f9665f823190c8f
Byron Peña <byronpenna@gmail.com>
2015-07-07 14:04:52 -0600

    front end repositorio

---
+++ IUS/IUS/Controllers/RepositorioController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// libreriras internas
+    using IUS.Models.page.Repositorio.Acciones;
+// librerias externas
+    using IUSLibs.TRL.Entidades;
+    using IUSLibs.LOGS;
+namespace IUS.Controllers
+{
+    public class RepositorioController : PadreController
+    {
+        #region "propiedades"
+            private RepositorioModel _model;
+            public int idPagina = (int)paginas.repositorio;
+        #endregion
+        #region "constructores"
+            public RepositorioController()
+            {
+                this._model = new RepositorioModel();
+            }
+        #endregion
+        #region "acciones url"
+            public ActionResult Index()
+            {
+                List<LlaveIdioma> traducciones;
+                try
+                {
+                    ViewBag.noticias = this._model.sp_adminfe_front_getTopNoticias(this._numeroNoticias);
+                    string lang = this.getUserLang();
+                    traducciones = this._model.getTraduccion(lang, this.idPagina);
+                    this.setTraduccion(traducciones);
+                }
+                catch (ErroresIUS x)
+                {
+                    return RedirectToAction("Unhandled", "Errors");
+                }
+                catch (Exception x)
+                {
+                    return RedirectToAction("Unhandled", "Errors");
+                }
+                return View();
+            }
+        #endregion
+    }
+}
---
+++ IUS/IUS/Models/page/Repositorio/Acciones/RepositorioModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// internas 
+    using IUS.Models.general;
+// externas 
+    using IUSLibs.LOGS;
+namespace IUS.Models.page.Repositorio.Acciones
+{
+    public class RepositorioModel:ModeloPadre
+    {
+
+    }
+}
