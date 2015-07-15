fbdb6e632183d94cf6532d6c36d97f064da8cebc
Byron Peña <byronpenna@gmail.com>
2015-07-15 17:38:44 -0600

    finalizacion de jornada

--- IUS/IUSBack/Scripts/Views/Home/Registro/functions.js
+++ IUS/IUSBack/Scripts/Views/Home/Registro/functions.js
@@ -1,5 +1,15 @@
 ﻿function frmRegistrar(frm) {
     actualizarCatalogo(RAIZ + "/Home/sp_secpu_addUsuario", frm, function (data) {
         console.log(data);
+        $(".spanResultado").removeClass("hidden");
+        if (data.estado) {
+            $(".spanResultado").addClass("spanSuccess");
+            $(".spanResultado").removeClass("spanError");
+            $(".spanResultado").empty().append("Registrado correctamente revise correo electronico para confirmar su cuenta");
+        } else {
+            $(".spanResultado").addClass("spanError");
+            $(".spanResultado").removeClass("spanSuccess");
+            $(".spanResultado").empty().append("Ocurrio un error");
+        }
     });
 }
