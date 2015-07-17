145248124ef0e2afb5f426b7a8d0b10a62c521e4
Byron Peña <byronpenna@gmail.com>
2015-07-17 11:49:24 -0600

    finalizacion de jornada

---
+++ IUS/IUSBack/Controllers/RepositorioCompartidoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IUSBack.Controllers
+{
+    public class RepositorioCompartidoController : Controller
+    {
+        //
+        // GET: /RepositorioCompartido/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
