1a308aa700849f396ae8e20f9109875e84aa2481
Byron Peña <byronpenna@gmail.com>
2015-09-01 12:43:36 -0600

    arreglos varios

--- IUS/IUSBack/Scripts/Views/Login/script.js
+++ IUS/IUSBack/Scripts/Views/Login/script.js
@@ -2,10 +2,12 @@
     // eventos 
         //click 
             $(document).on("click", "#btnUsuarioLogin", function () {
+                cambiarActivePestania("usuario");
                 $("#divLoginInvitado").removeClass("hidden");
                 $("#divLoginAdmin").addClass("hidden");
             })
             $(document).on("click", "#btnAdminLogin", function () {
+                cambiarActivePestania("admin");
                 $("#divLoginInvitado").addClass("hidden");
                 $("#divLoginAdmin").removeClass("hidden");
             })
