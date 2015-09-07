6a3efd15f9192569fc211268c83534c9e5be1372
Byron Peña <byronpenna@gmail.com>
2015-09-07 17:25:52 -0600

    avance informacion personas

---
+++ IUS/IUSLibs/RRHH/Entidades/EstadoCivil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IUSLibs.RRHH.Entidades
+{
+    public class EstadoCivil
+    {
+        #region "propiedades"
+            public int      _idEstadoCivil;
+            public string   _estadoCivil;
+        #endregion
+        #region "constructores"
+            public EstadoCivil(int idEstadoCivil)
+            {
+                this._idEstadoCivil = idEstadoCivil;
+            }
+            public EstadoCivil(int idEstadoCivil,string estadoCivil)
+            {
+                this._idEstadoCivil = idEstadoCivil;
+                this._estadoCivil = estadoCivil;
+            }
+        #endregion
+    }
+}
