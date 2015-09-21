dfecfa32cc93c17255f7bf90ab3c9bc9e57d6c86
Byron Peña <byronpenna@gmail.com>
2015-09-21 15:23:13 -0600

    Iniciales para gestion laboral

---
+++ IUS/IUSBack/Models/Page/GestionPersonas/acciones/GestionLaboralModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// librerias internas 
+    using IUSBack.Models.General;
+namespace IUSBack.Models.Page.GestionPersonas.acciones
+{
+    public class GestionLaboralModel:PadreModel
+    {
+        #region "propiedades"
+            
+        #endregion
+        #region "funciones"
+            #region "do"
+                
+            #endregion
+            #region "get"
+
+            #endregion
+        #endregion
+    }
+}
---
+++ IUS/IUSLibs/RRHH/Entidades/Laboral/LaboralPersona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// internas 
+    using IUSLibs.SEC.Entidades;
+namespace IUSLibs.RRHH.Entidades.Laboral
+{
+    public class LaboralPersona
+    {
+        #region "Propiedades"
+            public int              _idLaboralPersona;
+            public Empresa          _empresa;
+            public int              _inicio;
+            public int              _fin;
+            public Persona          _persona;
+            public string           _observaciones;
+            public CargoEmpresa     _cargo;
+        #endregion
+        #region "Constructores"
+            // basico
+                public LaboralPersona(int idLaboralPersona)
+                {
+                    this._idLaboralPersona = idLaboralPersona;
+                }
+            // full
+                public LaboralPersona(int idLaboralPersona,int idEmpresa,int inicio,int fin,int idPersona,string observaciones, int idCargo)
+                {
+                    // do 
+                        Empresa empresa     = new Empresa(idEmpresa);
+                        Persona persona     = new Persona(idPersona);
+                        CargoEmpresa cargo  = new CargoEmpresa(idCargo);
+                    // set 
+                        this._idLaboralPersona  = idLaboralPersona;
+                        this._empresa           = empresa;
+                        this._inicio            = inicio;
+                        this._fin               = fin;
+                        this._persona           = persona;
+                        this._observaciones     = observaciones;
+                        this._cargo             = cargo;
+                }
+
+        #endregion
+    }
+}
