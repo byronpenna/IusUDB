9b9df236da1db4ee2f3a0736f479811414658114
Byron Peña <byronpenna@gmail.com>
2015-10-03 09:09:40 -0600

    antes de traer rubros

--- IUS/IUSBack/Models/Page/RecursosHumanos/Acciones/RecursosHumanosModel.cs
+++ IUS/IUSBack/Models/Page/RecursosHumanos/Acciones/RecursosHumanosModel.cs
@@ -7,11 +7,13 @@ using System.Web;
 // Externas
     using IUSLibs.LOGS;
 // Externas
-    using IUSLibs.RRHH.Entidades.Formacion;
+    // entidades
+        using IUSLibs.RRHH.Entidades.Formacion;
+        using IUSLibs.RRHH.Entidades;
     // control
         using IUSLibs.RRHH.Control.Formacion;
         using IUSLibs.FrontUI.Control;
-        using IUSLibs.RRHH.Entidades;
+        using IUSLibs.RRHH.Control;
 namespace IUSBack.Models.Page.RecursosHumanos.Acciones
 {
     public class RecursosHumanosModel:PadreModel
@@ -30,12 +32,13 @@ namespace IUSBack.Models.Page.RecursosHumanos.Acciones
                     try
                     {
                         respuesta = new Dictionary<object, object>();
-                        ControlNivelCarrera controlCarrera  = new ControlNivelCarrera();
-                        ControlPais         controlPais     = new ControlPais();
-                        controles
+                        ControlNivelCarrera controlCarrera      = new ControlNivelCarrera();
+                        ControlPais         controlPais         = new ControlPais();
+                        ControlEstadoCivil  controlEstadoCivil  = new ControlEstadoCivil();
+                        //controles
                         respuesta.Add("nivelesTitulos", controlCarrera.sp_rrhh_getNivelesCarreras(idUsuarioEjecutor, idPagina));
                         respuesta.Add("paises", controlPais.sp_frontui_getPaises());
-                        respuesta.Add("estadoCiviles",)
+                        respuesta.Add("estadoCiviles", controlEstadoCivil.sp_rrhh_getEstadosCiviles());
                         return respuesta;
                     }
                     catch (ErroresIUS x)
--- IUS/IUSLibs/RRHH/Control/ControlEstadoCivil.cs
+++ IUS/IUSLibs/RRHH/Control/ControlEstadoCivil.cs
@@ -2,11 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+// manejo de datos
+    using System.Data.Sql;
+    using System.Data.SqlClient;
+    using System.Data;
+// internas 
+    // generales
+        using IUSLibs.BaseDatos;
+        using IUSLibs.GENERALS;
+        using IUSLibs.LOGS;
+    //----------
+        using IUSLibs.RRHH.Entidades;
 namespace IUSLibs.RRHH.Control
 {
-    public class ControlEstadoCivil
+    public class ControlEstadoCivil:PadreLib
     {
-
+        #region "funciones"
+            #region "do"
+            #endregion 
+            #region "get"
+                public List<EstadoCivil> sp_rrhh_getEstadosCiviles()
+                {
+                    List<EstadoCivil> estadosCiviles = null; EstadoCivil estadoCivil;
+                    SPIUS sp = new SPIUS("sp_rrhh_getEstadosCiviles");
+                    try
+                    {
+                        DataTableCollection tb = this.getTables(sp.EjecutarProcedimiento());
+                        if (this.resultadoCorrectoGet(tb))
+                        {
+                            if (tb[0].Rows.Count > 0)
+                            {
+                                estadosCiviles = new List<EstadoCivil>();
+                                foreach (DataRow row in tb[0].Rows)
+                                {
+                                    estadoCivil = new EstadoCivil((int)row["idEstadoCivil"], row["estado_civil"].ToString());
+                                    estadosCiviles.Add(estadoCivil);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            DataRow row = tb[0].Rows[0];
+                            ErroresIUS x = this.getErrorFromExecProcedure(row);
+                            throw x;
+                        }
+                    }
+                    catch (ErroresIUS x)
+                    {
+                        throw x;
+                    }
+                    catch(Exception x)
+                    {
+                        throw x;
+                    }
+                    return estadosCiviles;
+                }
+            #endregion
+        #endregion
     }
 }
