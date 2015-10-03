34692d22526fd8e73649cecbd758cd7b3bc83bc3
Byron Peña <byronpenna@gmail.com>
2015-10-03 08:14:44 -0600

    carga algunos elementos de busqueda

--- IUS/IUSBack/Controllers/Website/RecursosHumanosController.cs
+++ IUS/IUSBack/Controllers/Website/RecursosHumanosController.cs
@@ -29,6 +29,10 @@ namespace IUSBack.Controllers.Website
                     Usuario usuarioSession  = this.getUsuarioSesion();
                     ViewBag.selectedMenu    = 3; // menu seleccionado 
                     ViewBag.titleModulo     = "Recursos humanos";
+                    // objetos para pagina
+                    //ViewBag.nivelCarrera    = this._model.sp_rrhh_getNivelesCarreras(usuarioSession._idUsuario,this._idPagina);
+                    //ViewBag.paises          = this._model.
+                    ViewBag.objInicial      = this._model.cargaInicial(usuarioSession._idUsuario,this._idPagina);
                     ViewBag.menus           = this._model.sp_sec_getMenu(usuarioSession._idUsuario);
                 }
                 catch (ErroresIUS x)
--- IUS/IUSBack/Models/Page/RecursosHumanos/Acciones/RecursosHumanosModel.cs
+++ IUS/IUSBack/Models/Page/RecursosHumanos/Acciones/RecursosHumanosModel.cs
@@ -6,7 +6,12 @@ using System.Web;
     using IUSBack.Models.General;
 // Externas
     using IUSLibs.LOGS;
-
+// Externas
+    using IUSLibs.RRHH.Entidades.Formacion;
+    // control
+        using IUSLibs.RRHH.Control.Formacion;
+        using IUSLibs.FrontUI.Control;
+        using IUSLibs.RRHH.Entidades;
 namespace IUSBack.Models.Page.RecursosHumanos.Acciones
 {
     public class RecursosHumanosModel:PadreModel
@@ -19,7 +24,45 @@ namespace IUSBack.Models.Page.RecursosHumanos.Acciones
                 
             #endregion
             #region "get"
-            
+                public Dictionary<object, object> cargaInicial(int idUsuarioEjecutor,int idPagina)
+                {
+                    Dictionary<object, object> respuesta = null;
+                    try
+                    {
+                        respuesta = new Dictionary<object, object>();
+                        ControlNivelCarrera controlCarrera  = new ControlNivelCarrera();
+                        ControlPais         controlPais     = new ControlPais();
+                        controles
+                        respuesta.Add("nivelesTitulos", controlCarrera.sp_rrhh_getNivelesCarreras(idUsuarioEjecutor, idPagina));
+                        respuesta.Add("paises", controlPais.sp_frontui_getPaises());
+                        respuesta.Add("estadoCiviles",)
+                        return respuesta;
+                    }
+                    catch (ErroresIUS x)
+                    {
+                        throw x;
+                    }
+                    catch (Exception x)
+                    {
+                        throw x;
+                    }
+                }
+                /*public List<NivelTitulo> sp_rrhh_getNivelesCarreras(int idUsuarioEjecutor, int idPagina)
+                {
+                    try
+                    {
+                        ControlNivelCarrera control = new ControlNivelCarrera();
+                        return control.sp_rrhh_getNivelesCarreras(idUsuarioEjecutor, idPagina);
+                    }
+                    catch (ErroresIUS x)
+                    {
+                        throw x;
+                    }
+                    catch (Exception x)
+                    {
+                        throw x;
+                    }
+                }*/
             #endregion
         #endregion
     }
