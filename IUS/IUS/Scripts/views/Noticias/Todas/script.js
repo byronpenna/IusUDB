4d12d8324479ddb45393ab03a3677e57765c20d5
Byron Peña <byronpenna@gmail.com>
2015-10-15 15:40:27 -0600

    instalacion jquery ui
    asdqweqwe

--- IUS/IUS/Scripts/views/Noticias/Todas/script.js
+++ IUS/IUS/Scripts/views/Noticias/Todas/script.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
     // plugins
         //datePicker
+            $(".datePicker").datepicker({
+                dateFormat: "dd/mm/yy"
+            });
     // eventos
 })
