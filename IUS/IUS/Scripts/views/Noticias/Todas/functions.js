3cefd0acce52282987230dc626a15384060cc9cc
Byron Peña <byronpenna@gmail.com>
2015-10-19 08:45:08 -0600

    paginacion

--- IUS/IUS/Scripts/views/Noticias/Todas/functions.js
+++ IUS/IUS/Scripts/views/Noticias/Todas/functions.js
@@ -1,4 +1,26 @@
-﻿function getDivPost(post) {
+﻿function numPaginacion(frm) {
+    var target = $(".divTarjetasNoticias");
+    actualizarCatalogo(RAIZ + "/Noticias/sp_adminfe_front_getNoticiasPagina", frm, function (data) {
+        console.log(data);
+        var divPost = "";
+        if (data.estado) {
+            if (data.posts !== undefined && data.posts !== null && data.posts.length > 0) {
+                $.each(data.posts, function (i, post) {
+                    divPost += getDivPost(post);
+                })
+            }
+            target.empty().append(divPost);
+        }
+
+    }, function () {
+        target.empty().append("\
+            <div class='row text-center'>\
+                <img src='" + RAIZ + "/Content/images/generales/ajax-loader.gif' />\
+            </div>\
+        ")
+    })
+}
+function getDivPost(post) {
     var srcImg = IMG_GENERALES+"image.png";
     
     if (post.convertMiniatura !== null) {
--- IUS/IUS/Scripts/views/Noticias/Todas/script.js
+++ IUS/IUS/Scripts/views/Noticias/Todas/script.js
@@ -12,4 +12,13 @@
                 console.log(frm);
                 btnBuscar(frm);
             })
+            $(document).on("click", ".numPaginacion", function () {
+                var frm = {
+                    pagina: $(this).text(), // pagina 
+                    cn:     $(".txtHdRango").val()
+                }
+                console.log("formulario a enviar",frm);
+                numPaginacion(frm);
+            })
+            
 })
