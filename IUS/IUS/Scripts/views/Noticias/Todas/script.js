472ee72d6ade71a423631c67d3d42fa5d211bc80
Byron Peña <byronpenna@gmail.com>
2015-10-19 09:34:21 -0600

    paginacion de noticias

--- IUS/IUS/Scripts/views/Noticias/Todas/script.js
+++ IUS/IUS/Scripts/views/Noticias/Todas/script.js
@@ -12,12 +12,24 @@
                 console.log(frm);
                 btnBuscar(frm);
             })
+            
+            $(document).on("click", ".flecha", function () {
+                var padre = $(this).parents(".divContenedorNumeros");
+                var target = target.find(".grupoNumActivo");
+                if ($(this).hasClass("fechaLeft")) {
+
+                } else if($(this).hasClass("")){
+
+                }
+            })
             $(document).on("click", ".numPaginacion", function () {
                 var frm = {
                     pagina: $(this).text(), // pagina 
                     cn:     $(".txtHdRango").val()
                 }
-                console.log("formulario a enviar",frm);
+                console.log("formulario a enviar", frm);
+                $(".activeNum").removeClass("activeNum");
+                $(this).addClass("activeNum");
                 numPaginacion(frm);
             })
             
