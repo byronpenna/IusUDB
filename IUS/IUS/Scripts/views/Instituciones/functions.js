d3187ac3c2c938cc84b4f4e70159182f0867893f
Byron Peña <byronpenna@gmail.com>
2015-11-03 15:12:30 -0600

    instituciones :)

--- IUS/IUS/Scripts/views/Instituciones/functions.js
+++ IUS/IUS/Scripts/views/Instituciones/functions.js
@@ -36,34 +36,49 @@
         ";
         return tr;
     }
-// scripts 
-    function buscarContinente(frm) {
-        var target = $(".tbodyInstitucion");
-        actualizarCatalogo(RAIZ + "/Instituciones/sp_frontui_getInstitucionesByContinente", frm, function (data) {
-            console.log("D: :DP", data)
-            if (data.estado) {
-                var instituciones = data.instituciones.instituciones;
+    function getTrInstitucionNull() {
+        var tr = "\
+            <tr>\
+                <td colspan='3' class='text-center'>No hay instituciones para mostrar</td>\
+            </tr>\
+        ";
+        return tr;
+    }
+    // scripts 
+        function buscarContinente(frm) {
+            var target = $(".tbodyInstitucion");
+            actualizarCatalogo(RAIZ + "/Instituciones/sp_frontui_getInstitucionesByContinente", frm, function (data) {
+                console.log("D: :DP", data)
+                if (data.estado) {
+                    var instituciones = data.instituciones.instituciones;
                 
-                var tr = "";
-                console.log("instituciones", instituciones);
-                if(instituciones !== undefined && instituciones != null && instituciones.length > 0){
-                    console.log("entro aqui");
-                    $.each(instituciones, function (i,institucion) {
-                        tr += getTrInstitucion(institucion);
-                    });
+                    var tr = "";
+                    console.log("instituciones", instituciones);
+                    var hContinente = "";
+                    if (data.instituciones.continente !== undefined && data.instituciones.continente != null) {
+                        hContinente = data.instituciones.continente._continente;
+                    }
+                    if (instituciones !== undefined && instituciones != null && instituciones.length > 0) {
+                        console.log("entro aqui");
+                        $.each(instituciones, function (i,institucion) {
+                            tr += getTrInstitucion(institucion);
+                        });                        
+                    } else {
+                        tr = getTrInstitucionNull();
+                    }
+                
+                    target.empty().append(tr);
+                    $(".hContinente").empty().append(hContinente);
                 }
-                
-                //target.empty().append(tr);
-            }
             
-        }, function () {
-            var trLoading = "\
+            }, function () {
+                var trLoading = "\
             <tr>\
                 <td colspan='3' class='text-center'>\
                     <img src='" + IMG_GENERALES + "ajax-loader.gif'>\
                 </td>\
             </tr>\
             ";
-            target.empty().append(trLoading);
-        })
-    }
+                target.empty().append(trLoading);
+            })
+        }
