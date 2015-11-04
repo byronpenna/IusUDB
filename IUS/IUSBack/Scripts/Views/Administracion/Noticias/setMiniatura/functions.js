9c12903fd4bd4517593c5eb779be945a40b9c9b0
Byron Peña <byronpenna@gmail.com>
2015-11-04 17:21:37 -0600

    finalizacion de jornada

--- IUS/IUSBack/Scripts/Views/Administracion/Noticias/setMiniatura/functions.js
+++ IUS/IUSBack/Scripts/Views/Administracion/Noticias/setMiniatura/functions.js
@@ -1,8 +1,18 @@
-﻿function frmMiniatura(data,url,image){
-    accionAjaxWithImage(url, data, function (data) {
-        console.log("reespueta", data);
-        if (data.estado) {
-            $(".imgThumbnail").attr("src", image.src);
-        }
-    })
-}
+﻿// generics 
+    // jcrop 
+        function storeCoords(c) {
+            //console.log(c);
+            $(".x").val(c.x);
+            $(".y").val(c.y);
+            $(".imgAlto").val(c.h);
+            $(".imgAncho").val(c.w);
+        };
+// scripts 
+    function frmMiniatura(data, url, image) {
+        accionAjaxWithImage(url, data, function (data) {
+            console.log("reespueta", data);
+            if (data.estado) {
+                $(".imgThumbnail").attr("src", image.src);
+            }
+        })
+    }
