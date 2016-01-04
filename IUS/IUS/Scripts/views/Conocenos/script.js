3404eea6906cd16f31564b7a1cd60f522dfc0ea1
Byron Peña <byronpenna@gmail.com>
2016-01-04 10:51:36 -0600

    arreglos menores

--- IUS/IUS/Scripts/views/Conocenos/script.js
+++ IUS/IUS/Scripts/views/Conocenos/script.js
@@ -1,12 +1,15 @@
 ﻿$(document).ready(function () {
+    
     // eventos 
         // click 
             $(document).on("click", ".divImgCambio", function () {
+                console.log("Cambio D: D: D: ");
                 $(".divImgCambio").removeClass("selectedIco");
                 $(this).addClass("selectedIco");
                 var selecionado = parseInt($(this).attr("id"));
                 frm = { idSeleccion: selecionado };
                 divImgCambio(frm);
             })
-            
+    // iniciales
+            inicial();
 })
