ae54a9284ac54d8fdf9309ecdbbe0c0cfec41af7
Byron Peña <byronpenna@gmail.com>
2016-01-07 15:57:23 -0600

    finalizacion de jornada

--- IUS/IUSLibs/ADMINFE/Control/ControlConfiguraciones.cs
+++ IUS/IUSLibs/ADMINFE/Control/ControlConfiguraciones.cs
@@ -105,62 +105,62 @@ namespace IUSLibs.ADMINFE.Control
         #endregion
         #region "get"
             public Dictionary<object, object> sp_adminfe_getConfiguraciones(int idUsuarioEjecutor,int idPagina)
-        {
-            Dictionary<object, object> respuesta= null;
-            SPIUS sp = new SPIUS("sp_adminfe_getConfiguraciones");
-            sp.agregarParametro("idUsuarioEjecutor", idUsuarioEjecutor);
-            sp.agregarParametro("idPagina", idPagina);
-            Configuracion config = null;
-            List<RedSocial> redesSociales = null;
-            List<Valor> valores = null;
-            RedSocial redSocial;
-            Valor valor;
-            try
             {
-                DataTableCollection tb = this.getTables(sp.EjecutarProcedimiento());
-                if (this.resultadoCorrecto(tb))
+                Dictionary<object, object> respuesta= null;
+                SPIUS sp = new SPIUS("sp_adminfe_getConfiguraciones");
+                sp.agregarParametro("idUsuarioEjecutor", idUsuarioEjecutor);
+                sp.agregarParametro("idPagina", idPagina);
+                Configuracion config = null;
+                List<RedSocial> redesSociales = null;
+                List<Valor> valores = null;
+                RedSocial redSocial;
+                Valor valor;
+                try
                 {
+                    DataTableCollection tb = this.getTables(sp.EjecutarProcedimiento());
+                    if (this.resultadoCorrecto(tb))
+                    {
                     
-                    if (tb[1].Rows.Count > 0)
-                    {
-                        DataRow rowConfiguracion = tb[1].Rows[0];
-                        int idConfiguracion = Convert.ToInt32((bool)tb[1].Rows[0]["idConfiguracion"]);
-                        config = new Configuracion(idConfiguracion, (int)rowConfiguracion["id_idioma_fk"], rowConfiguracion["vision"].ToString(), rowConfiguracion["mision"].ToString(), rowConfiguracion["historia"].ToString());
-                    }
-                    if (tb[2].Rows.Count > 0)
-                    {
-                        redesSociales = new List<RedSocial>();
-                        foreach (DataRow row in tb[2].Rows)
+                        if (tb[1].Rows.Count > 0)
                         {
-                            redSocial = new RedSocial((int)row["idRedSocial"], row["nombre"].ToString(), row["enlace"].ToString(), row["clase_icono"].ToString());
-                            redesSociales.Add(redSocial);
+                            DataRow rowConfiguracion = tb[1].Rows[0];
+                            int idConfiguracion = Convert.ToInt32((bool)tb[1].Rows[0]["idConfiguracion"]);
+                            config = new Configuracion(idConfiguracion, (int)rowConfiguracion["id_idioma_fk"], rowConfiguracion["vision"].ToString(), rowConfiguracion["mision"].ToString(), rowConfiguracion["historia"].ToString());
                         }
-                    }
-                    if (tb[3].Rows.Count > 0)
-                    {
-                        valores = new List<Valor>();
-                        foreach (DataRow row in tb[3].Rows)
+                        if (tb[2].Rows.Count > 0)
                         {
-                            valor = new Valor((int)row["idValor"], row["valor"].ToString(),Convert.ToInt32((bool)row["id_config_fk"]));
-                            valores.Add(valor);
+                            redesSociales = new List<RedSocial>();
+                            foreach (DataRow row in tb[2].Rows)
+                            {
+                                redSocial = new RedSocial((int)row["idRedSocial"], row["nombre"].ToString(), row["enlace"].ToString(), row["clase_icono"].ToString());
+                                redesSociales.Add(redSocial);
+                            }
                         }
+                        if (tb[3].Rows.Count > 0)
+                        {
+                            valores = new List<Valor>();
+                            foreach (DataRow row in tb[3].Rows)
+                            {
+                                valor = new Valor((int)row["idValor"], row["valor"].ToString(),Convert.ToInt32((bool)row["id_config_fk"]));
+                                valores.Add(valor);
+                            }
+                        }
+                        respuesta = new Dictionary<object, object>();
+                        respuesta.Add("configuracion", config);
+                        respuesta.Add("redesSociales", redesSociales);
+                        respuesta.Add("valores", valores);
                     }
-                    respuesta = new Dictionary<object, object>();
-                    respuesta.Add("configuracion", config);
-                    respuesta.Add("redesSociales", redesSociales);
-                    respuesta.Add("valores", valores);
                 }
+                catch (ErroresIUS x)
+                {
+                    throw x;
+                }
+                catch (Exception x)
+                {
+                    throw x;
+                }
+                return respuesta;
             }
-            catch (ErroresIUS x)
-            {
-                throw x;
-            }
-            catch (Exception x)
-            {
-                throw x;
-            }
-            return respuesta;
-        }
         #endregion
     }
 }
---
+++ IUS/IUSLibs/ADMINFE/Control/ControlDatosIUS.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// manejo de datos
+    using System.Data.Sql;
+    using System.Data.SqlClient;
+    using System.Data;
+// librerias internas
+    using IUSLibs.BaseDatos;
+    using IUSLibs.GENERALS;
+    using IUSLibs.LOGS;
+    using IUSLibs.ADMINFE.Entidades;
+
+namespace IUSLibs.ADMINFE.Control
+{
+    public class ControlDatosIUS:PadreLib
+    {
+        #region "get"
+            public DatosIus sp_adminfe_front_getDatosIUS(string ip, int idPagina)
+            {
+                try
+                {
+                    DatosIus retorno = null;
+                    SPIUS sp = new SPIUS("sp_adminfe_front_getMonthEvents");
+                    sp.agregarParametro("ip", ip);
+                    sp.agregarParametro("idPagina", idPagina);
+                    DataTableCollection tb = this.getTables(sp.EjecutarProcedimiento());
+                    if (this.resultadoCorrectoGet(tb))
+                    {
+                        if (tb[0].Rows.Count > 0)
+                        {
+                            DataRow row = tb[0].Rows[0];
+
+                            retorno = new DatosIus((int)row["salesianos_mundo"], (int)row["paises_presencia"], (int)row["provincias"], (int)row["grupos_familia"]);
+                        }
+                        else
+                        {
+                            DataRow row = tb[0].Rows[0];
+                            ErroresIUS x = this.getErrorFromExecProcedure(row);
+                        }
+                    }
+                    return retorno;
+                }
+                catch (ErroresIUS x)
+                {
+                    throw x;
+                }
+                catch (Exception x)
+                {
+                    throw x;
+                }
+            }
+        #endregion
+    }
+}
