0440763073cbd849ada269fe8dd3ba0e2a817fd0
Byron Peña <byronpenna@gmail.com>
2016-01-26 13:11:55 -0600

    funcionamiento de validaicon de cuenta usuairo publico

---
+++ IUS/IUSLibs/SECPU/Entidades/CodigoVerificacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IUSLibs.SECPU.Entidades
+{
+    public class CodigoVerificacion
+    {
+        #region "propiedades"
+            public int              _idCodigoVerificacion;
+            public int              _numero;
+            public DateTime         _fechaVencimiento;
+            public UsuarioPublico   _usuarioPublico;
+        #endregion
+        #region "constructores"
+            public CodigoVerificacion(int idCodigoVerificacion,int numero,DateTime fechaVencimiento,int idUsuarioPublico)
+            {
+                this._idCodigoVerificacion  = idCodigoVerificacion;
+                this._numero                = numero;
+                this._fechaVencimiento      = fechaVencimiento;
+                this._usuarioPublico        = new UsuarioPublico(idUsuarioPublico);
+            }
+        #endregion
+    }
+}
