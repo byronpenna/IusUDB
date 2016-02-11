455b694584366f17b6300e20683e829a75577fc0
Byron Peña <byronpenna@gmail.com>
2016-02-11 14:07:27 -0600

    accesibilidad para lost pass

--- IUS/IUS/Scripts/views/Login/LostPass/functions.js
+++ IUS/IUS/Scripts/views/Login/LostPass/functions.js
@@ -1,10 +1,25 @@
 ﻿// scripts 
     function frmInvitado(frm) {
+        var target = $(".divMessageLogin");
         actualizarCatalogo(RAIZ + "/Login/sp_secpu_solicitarCambio", frm, function (data) {
             console.log("La data devuelta es", data);
             if (data.estado) {
-
+                mjs = "Correo enviado correctamente, revise su bandeja de entrada";
+                $(".txtEmail").val("");
+            } else {
+                if (data.error !== undefined && data.error != null && data.error._mostrar) {
+                    mjs = data.error;
+                } else {
+                    mjs = "Ocurrio un error";
+                }
             }
+            printMessage(target, mjs, data.estado);
+        }, function () {
+            target.empty().append("Espere por favor \
+                        <div class='row marginNull'>\
+                            <img src='" + IMG_GENERALES + "ajax-loader.gif'/>\
+                        </div>\
+            ");
         })
     }
     function cambiarPass(frm) {
