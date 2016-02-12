bdbe3a2f82ef6f5314bd07b7d7621386dbaab038
Byron Peña <byronpenna@gmail.com>
2016-02-12 11:41:00 -0600

    acesibilidad para cambiar contraseña

--- IUS/IUSBack/Scripts/Views/Login/ForgetPass/functions.js
+++ IUS/IUSBack/Scripts/Views/Login/ForgetPass/functions.js
@@ -1,8 +1,24 @@
 ﻿function frmInvitado(frm) {
     actualizarCatalogo(RAIZ + "Login/sp_usu_cambiarPassUsuario", frm, function (data) {
         console.log("La respuesta fue", data);
+        var target = $(".divMessageLogin");
+        var mjs = "";
         if (data.estado) {
-
+            mjs = "Contraseña fue cambiada exitosamente";
+        } else {
+            if (data.error !== undefined && data.error != null && data.error._mostrar) {
+                mjs = data.error.Message;
+            } else {
+                mjs = "Ocurrio un error";
+            }
         }
+        printMessage(target,mjs, data.estado);
+    }, function () {
+        target.empty().append("\
+            <div class='row marginNull'>\
+                Espere por favor\
+            </div>\
+            <img src='"+ IMG_GENERALES + "ajax-loader.gif'>\
+        ");
     })
 }
