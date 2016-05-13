65192d274095085e2e8e9ac57cef8fbc9cdbff10
Byron Peña <byronpenna@gmail.com>
2016-05-13 07:46:29 -0600

    Plantilla instituciones diseño

--- IUS/IUS/Scripts/views/Instituciones/Indexi/functions.js
+++ IUS/IUS/Scripts/views/Instituciones/Indexi/functions.js
@@ -1,1 +1,45 @@
-﻿
+﻿function buscarContinente(frm) {
+    var target = $(".tbodyInstitucion");
+    actualizarCatalogo(RAIZ + "/Instituciones/sp_frontui_getInstitucionesByContinente", frm, function (data) {
+        console.log("D: :DP", data)
+        if (data.estado) {
+            var instituciones = data.instituciones.instituciones;
+
+            var tr = "";
+            console.log("instituciones", instituciones);
+            var hContinente = "";
+            if (data.instituciones.continente !== undefined && data.instituciones.continente != null) {
+                hContinente = data.instituciones.continente._continente;
+            }
+            if (instituciones !== undefined && instituciones != null && instituciones.length > 0) {
+                console.log("entro aqui");
+                $.each(instituciones, function (i, institucion) {
+                    tr += getTrInstitucion(institucion);
+                });
+            } else {
+                tr = getTrInstitucionNull();
+            }
+
+            target.empty().append(tr);
+            $(".hContinente").empty().append(hContinente);
+        }
+
+    }, function () {
+        var trLoading = "\
+            <tr>\
+                <td colspan='3' class='text-center'>\
+                    <img src='" + IMG_GENERALES + "ajax-loader.gif'>\
+                </td>\
+            </tr>\
+            ";
+        target.empty().append(trLoading);
+    })
+}
+    function getTrInstitucionNull() {
+        var tr = "\
+                <tr >\
+                    <td colspan='3' class='text-center tdNull'>No hay instituciones para mostrar</td>\
+                </tr>\
+            ";
+        return tr;
+    }
--- IUS/IUS/Scripts/views/Instituciones/Indexi/script.js
+++ IUS/IUS/Scripts/views/Instituciones/Indexi/script.js
@@ -1,3 +1,15 @@
 ﻿$(document).ready(function () {
-
+    // eventos
+        // click
+            /*$(document).on("click", ".menuLateral li", function () {
+                $(this).addClass(".activeInstitucion");
+                $(this).find(".imgMap").attr("src", $(this).find(".txtHdRoja").val());
+            })*/
+            $(document).on("click", ".menuLateral li", function () {
+                var frm = {
+                    id: $(this).attr("id")
+                }
+                window.history.pushState({}, "Titulo", "/Instituciones/index/" + frm.id);
+                buscarContinente(frm);
+            })
 })
