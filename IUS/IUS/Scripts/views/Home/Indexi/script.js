a9f7fbb5f7d6cf97900bf2c9cc1878a485379465
Byron Peña <byronpenna@gmail.com>
2016-05-17 10:25:23 -0600

    optiomizaciones generales

--- IUS/IUS/Scripts/views/Home/Indexi/script.js
+++ IUS/IUS/Scripts/views/Home/Indexi/script.js
@@ -1,3 +1,8 @@
 ﻿$(document).ready(function () {
-    
+    console.log("Fue un click");
+    // click
+        $(document).on("click", ".bloqueLink", function () {
+
+            window.location.href = $(this).find(".txtHdUrlLink").val();
+        })
 })
