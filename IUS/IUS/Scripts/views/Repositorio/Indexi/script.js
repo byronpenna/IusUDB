c39d30ba83064762198beaf76f9752acdc26ab50
Byron Peña <byronpenna@gmail.com>
2016-05-20 08:42:42 -0600

    Sistema de navegacion en recursos

--- IUS/IUS/Scripts/views/Repositorio/Indexi/script.js
+++ IUS/IUS/Scripts/views/Repositorio/Indexi/script.js
@@ -1,3 +1,14 @@
 ﻿$(document).ready(function () {
+    // eventos 
+    $(document).on("click", ".menuLateral li", function () {
+        //$(this).addClass("activeRecurso");
 
+        $(".activeRecurso").find(".imgLateral").attr("src", $(".activeRecurso").find(".txtHdNormal").val());
+        $(".activeRecurso").removeClass("activeRecurso");
+        
+        $(this).find(".imgLateral").attr("src", $(this).find(".txtHdRoja").val());
+        $(this).addClass("activeRecurso");
+
+        window.history.pushState({}, "Titulo", "/Repositorio/index/");
+    })
 })
