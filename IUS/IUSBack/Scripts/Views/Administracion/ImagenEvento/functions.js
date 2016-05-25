355cd38274049ff89f797481f6fd1c2af5cf6879
Byron Peña <byronpenna@gmail.com>
2016-05-25 14:10:59 -0600

    antes de publicar

--- IUS/IUSBack/Scripts/Views/Administracion/ImagenEvento/functions.js
+++ IUS/IUSBack/Scripts/Views/Administracion/ImagenEvento/functions.js
@@ -15,10 +15,12 @@ function setFrmCoords(frm, imgQuery) {
 function frmImagenEvento(data, url, image, jcrop_api) {
     var targetImg = $(".imgThumbnail"), boton = $(".btnSubir");
     accionAjaxWithImage(url, data, function (data) {
-        console.log("respuesta", data);
+        console.log("respuesta despues de setear", data);
         if (data.estado) {
             jcrop_api.destroy();
-            //$(".imgThumbnail").attr("src", image.src);
+            $(".imgThumbnail").attr("src", RAIZ + data.imagen);
+            targetImg.attr("style", "");
+            //
             /*targetImg.attr("src", RAIZ + "GestionInstituciones/getImageThumbLogo/" + data.id);
             console.log("url a poner es", RAIZ + "GestionInstituciones/getImageThumbLogo/" + data.id);
             targetImg.attr("style", "");*/
