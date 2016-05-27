0e66546a8b2f244950b7dd85e9cff59c33b7eb8d
Byron Peña <byronpenna@gmail.com>
2016-05-27 08:19:16 -0600

    Antes de publicacion

---
+++ IUS/IUSBack/Controllers/Configuraciones/ConfigRepoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IUSBack.Controllers.Configuraciones
+{
+    public class ConfigRepoController : Controller
+    {
+        //
+        // GET: /ConfigRepo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
