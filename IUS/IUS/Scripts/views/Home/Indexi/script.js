9369baeca3d4481dbce558b85f4751961e665106
Byron Peña <byronpenna@gmail.com>
2016-05-31 07:55:39 -0600

    funciona slider

--- IUS/IUS/Scripts/views/Home/Indexi/script.js
+++ IUS/IUS/Scripts/views/Home/Indexi/script.js
@@ -1,8 +1,17 @@
 ﻿$(document).ready(function () {
-    console.log("Fue un click");
-    // click
-        $(document).on("click", ".bloqueLink", function () {
-
-            window.location.href = $(this).find(".txtHdUrlLink").val();
-        })
+    //console.log("Fue un click");
+    // Eventos
+        // click
+            $(document).on("click", ".bloqueLink", function () {
+                window.location.href = $(this).find(".txtHdUrlLink").val();
+            })
+            $(document).on("click", ".navBtn", function () {
+                if (intervalo != null) {
+                    clearInterval(intervalo);
+                }
+                direccion = $(this).attr("direccion"); //0 izquierda 1 derecha
+                divSlider = $(this).parents(".slider");
+                navBtn(divSlider, direccion);
+                startSlider();
+            })
 })
