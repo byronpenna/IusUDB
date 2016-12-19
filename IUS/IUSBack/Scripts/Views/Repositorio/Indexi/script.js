debd0665aec60c3634f72c46730ad27de47906fd
Byron Peña <byronpenna@gmail.com>
2016-12-19 09:30:17 -0600

    Creación de cuadro modal con nuevo diseño

--- IUS/IUSBack/Scripts/Views/Repositorio/Indexi/script.js
+++ IUS/IUSBack/Scripts/Views/Repositorio/Indexi/script.js
@@ -1,3 +1,32 @@
 ﻿$(document).ready(function () {
+    // eventos 
+        // click 
+            // Para subir
+                $(document).on("click", ".icoSubirFichero", function (e) {
+                    e.preventDefault();
+                    $(".divUpload").fadeIn(400, function () {
 
+                    });
+                })
+                $(document).on("click", ".contenedorUpload", function (e) {
+                    e.stopPropagation();
+                });
+                $(document).on("click", ".divUpload", function (e) {
+                    $(this).fadeOut();
+                })
+                $(document).on("click", ".closeModal", function (e) {
+                    $(".divUpload").click();
+                })
+            // para menu 
+                $(document).on("click", ".NewActionLi", function () {
+                    var elemento = $(this).find(".ulSub");
+                    if (elemento.is(":visible")) {
+                        elemento.hide();
+                        console.log("Mostrar");
+                    } else {
+                        //$(this).find(".ulSub").hide();
+                        elemento.show();
+                        console.log("Ocultar");
+                    }
+                });
 })
