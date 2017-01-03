728d67e5e03be49e44f37f8053eb0774921e46ee
Byron Peña <byronpenna@gmail.com>
2017-01-03 10:34:07 -0600

    Devolver funcionalidad a botón eliminar archivo publico.

--- IUS/IUSBack/Scripts/Views/RepositorioPublico/Indexi/functions.js
+++ IUS/IUSBack/Scripts/Views/RepositorioPublico/Indexi/functions.js
@@ -1,4 +1,11 @@
-﻿function btnEliminarCarpeta(frm, tr) {
+﻿function btnEliminarArchivo(frm,tr) {
+    actualizarCatalogo(RAIZ + "/RepositorioPublico/sp_repo_removeShareArchivoPublico", frm, function (data) {
+        if (data.estado) {
+            tr.remove();
+        }
+    })
+}
+function btnEliminarCarpeta(frm, tr) {
     actualizarCatalogo(RAIZ + "/RepositorioPublico/sp_repo_deleteCarpetaPublica", frm, function (data) {
         console.log(data);
         if (data.estado) {
--- IUS/IUSBack/Scripts/Views/RepositorioPublico/Indexi/script.js
+++ IUS/IUSBack/Scripts/Views/RepositorioPublico/Indexi/script.js
@@ -1,11 +1,24 @@
 ﻿$(document).ready(function () {
-    $(document).on("click", ".btnEliminarCarpeta", function () {
-        var tr = $(this).parents("tr");
-        var x = confirm("¿Esta seguro que desea eliminar esta carpeta?");
-        frm = { idCarpeta: tr.find(".txtHdIdCarpeta").val() }
-        if (x) {
-            btnEliminarCarpeta(frm, tr)
-        }
-    })
+    // eliminar 
+    
+        $(document).on("click", ".btnEliminarArchivo", function () {
+            var x = confirm("¿Esta seguro que desea dejar de compartir este archivo?");
+            if (x) {
+                var tr = $(this).parents("tr");
+                var frm = {
+                    idArchivoPublico: tr.find(".txtHdIdArchivoPublico").val()
+                }
+                console.log("Formulario a enviar es: ", frm); 
+                btnEliminarArchivo(frm, tr);
+            }
+        })
+        $(document).on("click", ".btnEliminarCarpeta", function () {
+            var tr = $(this).parents("tr");
+            var x = confirm("¿Esta seguro que desea eliminar esta carpeta?");
+            frm = { idCarpeta: tr.find(".txtHdIdCarpeta").val() }
+            if (x) {
+                btnEliminarCarpeta(frm, tr)
+            }
+        })
     
 })
